e6599caeaae338a91cbbfa8d3caecb447afd39a6
astro <astro.monitor@gmail.com>
2012-11-27 20:46:51 +0900

    modify user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ require 'digest'
 
 class User < ActiveRecord::Base
 	attr_accessor :password
-	attr_accessible :name, :email, :password, :possword_confirmation
+	attr_accessible :name, :email, :password, :password_confirmation
 
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
