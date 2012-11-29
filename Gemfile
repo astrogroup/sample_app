c89d5d2d93615550062a2ad0eb30b15c5080d029
astro <astro.monitor@gmail.com>
2012-11-29 20:39:45 +0900

    Done with user edit/update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.0.7'
 gem 'gravatar_image_tag'
+gem 'will_paginate', '3.0.pre2'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
@@ -10,6 +11,7 @@ group :development do
 	gem 'rspec-rails', '2.5.0'
 	gem 'annotate', '2.4.0'
 	gem 'sqlite3'
+	gem 'faker', '0.3.1'
 end
 
 group :test do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,25 @@
 class UsersController < ApplicationController
-	def new
+
+	before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
+	before_filter :correct_user, :only => [:edit, :update]
+	before_filter :admin_user,:only => :destroy
+
+	def new #GET new
 		@user = User.new
 		@title = "Sign up"
 	end
 
-	def show
+	def index #GET
+		@title = "All users"
+		@users = User.paginate(:page => params[:page])
+	end
+
+	def show #GET :id
 		@user = User.find(params[:id])
 		@title = @user.name
 	end
 
-	def create
+	def create #POST
 		@user = User.new(params[:user])
 		if @user.save
 			sign_in @user
@@ -22,4 +32,40 @@ class UsersController < ApplicationController
 			render 'new'
 		end
 	end
+
+	def edit #PUT :id/edit
+		@user = User.find(params[:id])
+		@title = "Edit user"
+	end
+
+	def update #PUT :id
+		@user = User.find(params[:id])
+		if @user.update_attributes(params[:user])
+			flash[:success] = "Profile update"
+			redirect_to @user
+		else
+			@title = "Edit user"
+			render 'edit'
+		end
+	end
+
+	def destroy
+		User.find(params[:id]).destroy
+		flash[:success] = "User destroyed"
+		redirect_to users_path
+	end
+
+	private
+		def authenticate
+			deny_access unless signed_in?
+		end
+
+		def correct_user
+			@user = User.find(params[:id])
+			redirect_to(root_path) unless current_user?(@user)
+		end
+
+		def admin_user
+			redirect_to(root_path) unless current_user.admin?
+		end
 end
